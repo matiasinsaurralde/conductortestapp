dd1d62950dae21b33b597cbd676820ba681bc10f
Matias Insaurralde <matias@insaurral.de>
2022-07-19 02:19:18 -0400

    Initial commit

---
+++ tests/app_test.py
@@ -0,0 +1,18 @@
+from app.app import app
+
+def test_healthz():
+    tester = app.test_client()
+    response = tester.get("/healthz", content_type="html/text")
+    assert response.status_code == 402
+
+def test_get_user():
+    tester = app.test_client()
+    response = tester.get("/v1/user", content_type="application/json", headers={
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE1NTQ3NTU1NTUsImV4cCI6MjU1NDc1NTUwMCwiaWF0IjoxNTU0NzU1NTAwLCJqdGkiOiI5ZmRmMGE2Ni00YzllLTRlOTktODc4MC05YjdlOTNlMjFlMjciLCJ1c2VyX2lkIjoiMTA1YjM1MTgtNjQ2ZC00NjNlLWFkZGEtZDJiOTM5YzJkMDZkIiwidXNlcl9mdWxsX25hbWUiOiJCZXJ0cmFtIEdpbGZveWxlIiwidXNlcl9lbWFpbCI6Im51bGxAcGllZHBpcGVyLmNvbSJ9.-A8Gx18iTikKpedcxDlgcc7D8GMWFix0709Vfpbo1SI'
+    })
+    assert response.status_code == 200
+
+def test_get_user_forbidden():
+    tester = app.test_client()
+    response = tester.get("/v1/user", content_type="application/json")
+    assert response.status_code == 403
